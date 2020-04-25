bff6cb982873feb59d1b26dcb7cf7eb180c247f3
sabertooth9 <2013nuhash@gmail.com>
2020-04-26 04:31:47 +0600

    Greedy AI added

--- app/src/main/java/com/sabertooth/flood_puzzle/AdapterGrid.java
+++ app/src/main/java/com/sabertooth/flood_puzzle/AdapterGrid.java
@@ -3,8 +3,6 @@ package com.sabertooth.flood_puzzle;
 
 import android.content.Context;
 import android.graphics.Color;
-import android.util.AttributeSet;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,19 +10,14 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.widget.AppCompatButton;
 import androidx.recyclerview.widget.RecyclerView;
-
-import static java.lang.Math.floor;
-import static java.lang.Math.max;
-import static java.lang.Math.min;
 
 class AdapterCell extends RecyclerView.ViewHolder {
     LinearLayout cx;
     AppCompatButton button;
 
-    public AdapterCell(@NonNull View itemView) {
+    AdapterCell(@NonNull View itemView) {
         super(itemView);
         button = itemView.findViewById(R.id.cellbutton);
         cx = itemView.findViewById(R.id.listCellView);
@@ -32,15 +25,15 @@ class AdapterCell extends RecyclerView.ViewHolder {
 }
 
 public class AdapterGrid extends RecyclerView.Adapter<AdapterCell> {
-    Context appContext;
-    FloodMaker FM;
-    int[] cellColor = {Color.BLUE, Color.DKGRAY, Color.YELLOW, Color.RED, Color.GREEN, Color.CYAN, Color.MAGENTA, Color.WHITE};
-    TextView result;
-    int count = 0;
-    boolean gameover = false;
-    int gamemode;
+    private Context appContext;
+    private FloodMaker FM;
+    private int[] cellColor = {Color.BLUE, Color.DKGRAY, Color.YELLOW, Color.RED, Color.GREEN, Color.CYAN, Color.MAGENTA, Color.WHITE};
+    private TextView result;
+    private int count = 0;
+    private boolean gameover = false;
+    private int gamemode;
 
-    public AdapterGrid(Context appContext, FloodMaker FM, TextView _res, int gamemode) {
+    AdapterGrid(Context appContext, FloodMaker FM, TextView _res, int gamemode) {
         this.appContext = appContext;
         this.FM = FM;
         this.gameover = false;
@@ -62,56 +55,52 @@ public class AdapterGrid extends RecyclerView.Adapter<AdapterCell> {
         int idx = FM.colorValue(position);
         final int curCellColor = cellColor[idx];
 
-        //  int x_st = FM.StartPositionX(position);
-        // int y_st = FM.StartPositionY(position);
-        //int x_en = FM.EndPositionX(position);
-        //int y_en = FM.EndPositionY(position);
         holder.cx.setBackgroundColor(curCellColor);
         holder.button.setBackgroundColor(curCellColor);
-        //holder.button.setText("NICE"+position);
-        //holder.button.redraw(x_st,y_st,x_en,y_en,curCell);
         if (!gameover) {
             holder.button.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     int maincell = FM.colorValue(0);
                     int curCell = FM.colorValue(position);
-                    if (gamemode != 0) count++;
-                    else if(maincell!=curCell)count++;
+                    boolean aiwon = false;
                     if (maincell != curCell) {
+                        count++;
                         FM.processMove(curCell);
                         notifyDataSetChanged();
-                    }
-                    if (gamemode == 1) {
-                        if (count % 2 == 1)
-                            result.setText("Player 2 Turn");
-                        else {
-                            result.setText("Player 1 Turn");
-                        }
-                    } else if (gamemode == 2) {
-                        if (count % 2 == 1)
-                            result.setText("AI Turn");
-                        else {
-                            result.setText("Player 1 Turn");
-                        }
-                    } else {
-                        result.setText("Total Moves " + count);
-                    }
-                    gameover = FM.gameFinished();
-                    if (gameover) {
-                        if (gamemode == 0) {
-                            result.setText("Player Finished with " + count + " moves");
-                        } else if (gamemode == 1) {
-                            if (count % 2 == 1) {
-                                result.setText("Player 1 Wins");
-                            } else {
-                                result.setText("Player 2 Wins");
+                        if (gamemode == 1) {
+                            if (count % 2 == 1)
+                                result.setText("Player 2 Turn");
+                            else {
+                                result.setText("Player 1 Turn");
                             }
                         } else if (gamemode == 2) {
-                            if (count % 2 == 1) {
-                                result.setText("Player 1 Wins");
-                            } else {
-                                result.setText("AI 2 Wins");
+
+                            if (!FM.gameFinished()) {
+                                FM.AI_Move();
+                                notifyDataSetChanged();
+                                if (FM.gameFinished()) aiwon = true;
+                            }
+                            result.setText("Player Turn");
+                        } else {
+                            result.setText("Total Moves " + count);
+                        }
+                        gameover = FM.gameFinished();
+                        if (gameover) {
+                            if (gamemode == 0) {
+                                result.setText("Player Finished with " + count + " moves");
+                            } else if (gamemode == 1) {
+                                if (count % 2 == 1) {
+                                    result.setText("Player 1 Wins");
+                                } else {
+                                    result.setText("Player 2 Wins");
+                                }
+                            } else if (gamemode == 2) {
+                                if (!aiwon) {
+                                    result.setText("Player 1 Wins");
+                                } else {
+                                    result.setText("AI Wins");
+                                }
                             }
                         }
                     }
--- app/src/main/java/com/sabertooth/flood_puzzle/FloodMaker.java
+++ app/src/main/java/com/sabertooth/flood_puzzle/FloodMaker.java
@@ -1,101 +1,145 @@
 package com.sabertooth.flood_puzzle;
-import android.graphics.Color;
+
 import android.util.Log;
 import android.util.Pair;
 
-import java.util.Random;
+class FloodMaker {
+    private int row, col;
+    private int[][] grid;
+    private int[][] ai_grid;
+    private int[][] visited;
+    private int[] cnt_map;
+    private int ai_cnt = 0;
+    private int[] dx = {-1, 1, 0, 0};
+    private int[] dy = {0, 0, -1, 1};
+    private int distinct_col;
 
-public class FloodMaker {
-    int row,col;
-    int[][] grid;
-    int[][] visited;
-    //int[][] X_st,X_en,Y_st,Y_en;
-    int dx[] = {-1,1,0,0};
-    int dy[] = {0,0,-1,1};
-    int distinct_col;
-    FloodMaker(int x,int y,int color_cnt){
+    FloodMaker(int x, int y, int color_cnt) {
         row = x;
         col = y;
         distinct_col = color_cnt;
+        cnt_map = new int[color_cnt + 2];
         grid = new int[x][y];
+        ai_grid = new int[x][y];
         visited = new int[x][y];
-     //   X_st = new int[x][y];
-      //  X_en = new int[x][y];
-       // Y_st = new int[x][y];
-        //Y_en = new int[x][y];
-        for(int i=0;i<x;i++){
-            for(int j=0;j<y;j++){
-                grid[i][j] = (int) (Math.random()*distinct_col);
-               // Log.e("GRID VALUE ",i+" "+j+" "+grid[i][j]);
-                assert (grid[i][j]<8);
+        for (int i = 0; i <= color_cnt; i++) cnt_map[i] = 0;
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                grid[i][j] = (int) (Math.random() * distinct_col);
+                ai_grid[i][j] = grid[i][j];
                 visited[i][j] = 0;
-          //      X_st[i][j] = j*sq_size;
-           //     X_en[i][j] = (j+1)*sq_size;
-            //    Y_st[i][j] = i*sq_size;
-             //   Y_en[i][j] = (i+1)*sq_size;
+                cnt_map[grid[i][j]]++;
             }
         }
     }
-    void runDfs(int x,int y,int color,int newColor){
+
+    private void runDfs(int x, int y, int color, int newColor) {
+        cnt_map[grid[x][y]]--;
         visited[x][y] = 1;
-        grid[x][y]= newColor;
-        for(int i=0;i<4;i++){
-            int px = dx[i]+x;
-            int py = dy[i]+y;
-            if(px>-1 && py>-1 && px<row && py<col && visited[px][py]==0 && (grid[px][py]==color || grid[px][py]==newColor)){
-                runDfs(px,py,color,newColor);
+        grid[x][y] = newColor;
+        cnt_map[grid[x][y]]++;
+        for (int i = 0; i < 4; i++) {
+            int px = dx[i] + x;
+            int py = dy[i] + y;
+            if (px > -1 && py > -1 && px < row && py < col && visited[px][py] == 0 && (grid[px][py] == color || grid[px][py] == newColor)) {
+                runDfs(px, py, color, newColor);
             }
         }
     }
-    void processMove(int color){
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
+
+    void processMove(int color) {
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
                 visited[i][j] = 0;
             }
         }
-        runDfs(0,0,grid[0][0],color);
+        runDfs(0, 0, grid[0][0], color);
     }
-    boolean gameFinished(){
-        boolean ok = true;
-        for(int i=0;i<row && ok;i++){
-            for(int j=0;j<col && ok;j++){
-                if(grid[i][j]!=grid[0][0]){
-                    ok = false;
-                    break;
-                }
+
+    boolean gameFinished() {
+        for (int k = 0; k < row; k++) {
+            for (int j = 0; j < col; j++) {
+                if (grid[k][j] != grid[0][0]) return false;
             }
         }
-        return ok;
+        return true;
     }
-    int count(){
-        return row*col;
+
+    void print_grid() {
+        StringBuilder x = new StringBuilder();
+        x.append("\n");
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                x.append(grid[i][j]);
+                x.append(" ");
+            }
+            x.append("\n");
+        }
+        Log.e("DEBUG", x.toString());
     }
-    int encode(int x,int y){
-        return x*col+y;
+
+    int count() {
+        return row * col;
     }
-    Pair<Integer,Integer> decode(int i){
-        return  Pair.create(i/col,i%col);
+
+    private int encode(int x, int y) {
+        return x * col + y;
     }
-    int colorValue(int i){
-        Pair<Integer,Integer> x =decode(i);
-        Log.e("LOG",x.first+" "+x.second);
+
+    private Pair<Integer, Integer> decode(int i) {
+        return Pair.create(i / col, i % col);
+    }
+
+    int colorValue(int i) {
+        Pair<Integer, Integer> x = decode(i);
+        //Log.e("LOG", x.first + " " + x.second);
         if ((grid[x.first][x.second] >= 8)) throw new AssertionError();
         return grid[x.first][x.second];
     }
-//    int StartPositionX(int i){
-//        Pair<Integer,Integer> x =decode(i);
-//        return X_st[x.first][x.second];
-//    }
-//    int StartPositionY(int i){
-//        Pair<Integer,Integer> x =decode(i);
-//        return Y_st[x.first][x.second];
-//    }
-//    int EndPositionY(int i){
-//        Pair<Integer,Integer> x =decode(i);
-//        return Y_en[x.first][x.second];
-//    }
-//    int EndPositionX(int i){
-//        Pair<Integer,Integer> x =decode(i);
-//        return X_en[x.first][x.second];
-//    }
+
+    private void greedy_ai_dfs(int x, int y, int color, int newColor) {
+        visited[x][y] = 1;
+        ai_cnt++;
+        ai_grid[x][y] = newColor;
+
+        for (int i = 0; i < 4; i++) {
+            int px = dx[i] + x;
+            int py = dy[i] + y;
+            if (px > -1 && py > -1 && px < row && py < col && visited[px][py] == 0 && (ai_grid[px][py] == color || ai_grid[px][py] == newColor)) {
+                greedy_ai_dfs(px, py, color, newColor);
+            }
+        }
+    }
+
+    private int greedy_AI_move() {
+        ai_cnt = 0;
+        int maxi = 0;
+        int colidx = -1;
+        for (int i = 0; i < distinct_col; i++) {
+            if (i == grid[0][0]) continue;
+            if (cnt_map[i] == 0) continue;
+            ai_grid = grid.clone();
+            ai_cnt = 0;
+
+            for (int k = 0; k < row; k++) {
+                for (int j = 0; j < col; j++) {
+                    visited[k][j] = 0;
+                }
+            }
+            greedy_ai_dfs(0, 0, grid[0][0], i);
+            if (maxi < ai_cnt) {
+                maxi = ai_cnt;
+                colidx = i;
+            }
+        }
+        return colidx;
+    }
+
+    void AI_Move() {
+        int idx = greedy_AI_move();
+        Log.e("AI MOVE", idx + "");
+        if (idx == -1) return;
+        processMove(idx);
+        // runDfs(0,0,grid[0][0],idx);
+    }
 }
